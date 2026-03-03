a3e1fbb1d7ce50da000fdb1d180444820fa1feca
eshake11 <groshev.andrey17@gmail.com>
2026-03-03 12:05:10 +0200

    Add strict types

--- src/Domain/Api/Dto/User/Response/UserResponse.php
+++ src/Domain/Api/Dto/User/Response/UserResponse.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Domain\Api\Dto\User\Response;
 
 class UserResponse
--- src/Domain/Api/User/UserDataConverter.php
+++ src/Domain/Api/User/UserDataConverter.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Domain\Api\User;
 
 use App\Domain\Api\Dto\User\Response\UserResponse;
